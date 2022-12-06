212bea413c663bb88679a1ffa70e70363bd744de
brandon riness <briness7521@gmail.com>
2022-12-06 10:31:54 -0500

    I have changed the format of my text

--- bestfriend.py
+++ bestfriend.py
@@ -1,4 +1,42 @@
-first_name = "Brandon"
-last_name = "Riness"
-print(f"Hello {first_name} {last_name}, you are lonely...")
+print("=========================")
+print("Best Friend Maker")
+print("=========================")
+print()
+print("Hi, I'm the computer.")
+
+name = input("What's your name? ")
+
+print(f"Hi, {name}!")
+
+color = input("What's your favorite color? ")
+
+print(f"{color}")
+
+print(f"No Way! My favorite color is also {color}!")
+
+show = input("What's your favorite show? ")
+
+print(f"{show}")
+
+print(f"What!?! My favorite show is also {show}!")
+
+sport = input("What's your favorite sport? ")
+
+print(f"{sport}")
+
+print(f"GET OUT OF HERE!!! My favorite sport is also {sport}")
+
+pizza = input("What's your leats favorite pizza topping? ")
+
+print (f"{pizza}")
+
+print(f"I dislike {pizza} on my pizza too! Just like you!")
+
+fav_class = input("What is your favorite class? ")
+
+print(f"{fav_class}")
+
+print(f"Wow my favorite class is {fav_class} as well!")
+print("We like all the same things! We should become best friends!")
+
 
