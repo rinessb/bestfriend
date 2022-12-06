c9ea56163dd95354beb0e46c2cc4482ceee92038
brandon riness <briness7521@gmail.com>
2022-12-06 10:45:17 -0500

    I have changed the format of my text

--- bestfriend.py
+++ bestfriend.py
@@ -10,33 +10,21 @@ print(f"Hi, {name}!")
 
 color = input("What's your favorite color? ")
 
-print(f"{color}")
-
 print(f"No Way! My favorite color is also {color}!")
 
 movie = input("What's your favorite movie? ")
-
-print(f"{movie}")
 
 print(f"What!?! My favorite show is also {movie}!")
 
 game = input("What's your favorite sport? ")
 
-print(f"{game}")
-
 print(f"GET OUT OF HERE!!! My favorite sport is also {game}!")
 
 pizza = input("What's your leats favorite pizza topping? ")
-
-print (f"{pizza}")
 
 print(f"I dislike {pizza} on my pizza too! Just like you!")
 
 fav_class = input("What is your favorite class? ")
 
-print(f"{fav_class}")
-
 print(f"Wow my favorite class is {fav_class} as well!")
 print("We like all the same things! We should become best friends!")
-
-
